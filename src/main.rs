6ae2571993a08de8371f3271099ccc610055c19c
Han Han <haannn@qq.com>
2020-10-24 17:51:21 +0800

    add test support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 mod vga_buffer;
 
@@ -15,6 +26,16 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Something is wrong");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion..");
+    assert_eq!(1, 1);
+    println!("[Ok]");
 }
